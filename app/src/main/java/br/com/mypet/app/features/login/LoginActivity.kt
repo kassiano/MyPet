076cb0ba9c5ef4ef710ca7084fcb7ce8a8b8c979
Manu <sandramarinhos78@gmail.com>
2020-11-22 13:37:12 -0300

    criado o model e o layout

---
+++ app/src/main/java/br/com/mypet/app/features/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.mypet.app.features.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.mypet.app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
