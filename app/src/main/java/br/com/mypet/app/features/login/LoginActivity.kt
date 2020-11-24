8414b753461ca76278c67fb4963ede5032ab69a5
Manu <sandramarinhos78@gmail.com>
2020-11-24 01:04:41 -0300

    mudando as view

--- app/src/main/java/br/com/mypet/app/features/login/LoginActivity.kt
+++ app/src/main/java/br/com/mypet/app/features/login/LoginActivity.kt
@@ -3,10 +3,22 @@ package br.com.mypet.app.features.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.mypet.app.R
+import br.com.mypet.app.features.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+
+    fun registerUser(){
+        btnRegisterUser.setOnClickListener {
+            startActivity<RegisterActivity>()
+        }
+
     }
 }
